19c9104115d818fa47379a0d3f947ab75976cdf3
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-11 20:54:45 +0300

    Add ability to delete joke from bookmarks

--- Scherzo/ViewController/BookMarkTableViewController.swift
+++ Scherzo/ViewController/BookMarkTableViewController.swift
@@ -61,17 +61,21 @@ class BookMarkTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            let delegate = UIApplication.shared.delegate as! AppDelegate
+            let context = delegate.persistentContainer.viewContext
+            context.delete(array[indexPath.row])
+            array.remove(at: indexPath.row)
             // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            
+            
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
