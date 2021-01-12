8480c49a75f2cbc0890bc83b49bc3060ba09228f
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-12 09:48:06 +0300

    Clean JokeDetail VC

--- Scherzo/ViewController/JokeDetailViewController.swift
+++ Scherzo/ViewController/JokeDetailViewController.swift
@@ -9,26 +9,26 @@ import UIKit
 
 class JokeDetailViewController: UIViewController {
     
+    // MARK: - IBOutlets
+    
     @IBOutlet weak var setupLine: UILabel!
     @IBOutlet weak var punchLine: UILabel!
+    
+    // MARK: - Public Properties
+    
     var joke: Joke?
-
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupLine.text = joke?.setup
-        punchLine.text = joke?.punchline
-        // Do any additional setup after loading the view.
+        setJoke()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Private Methods
+    
+    private func setJoke(){
+        setupLine.text = joke?.setup
+        punchLine.text = joke?.punchline
     }
-    */
-
 }
