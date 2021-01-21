7fe6c5a6e77e5c1739362d16c85d7c09ab79628d
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-21 21:46:19 +0300

    Add color constants

---
+++ Scherzo/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  Scherzo
+//
+//  Created by Mike Shevelinsky on 21.01.2021.
+//
+
+import UIKit
+
+
+enum Colors {
+    static let blue = UIColor(red: 35.0 / 255.0, green: 201.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
+    static let lightBlue = UIColor(red: 124.0 / 255.0, green: 198.0/255.0, blue: 254.0 / 255.0, alpha: 1.0)
+    static let lavander = UIColor(red: 204.0 / 255.0, green: 213.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
+
+}
