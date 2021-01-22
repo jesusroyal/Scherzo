c617ee11aa0c105aa0a0484593001f26725c5779
Mike Shevelinsky <mikedalvik@yandex.ru>
2021-01-22 15:38:04 +0300

    Add new colours

--- Scherzo/Colors.swift
+++ Scherzo/Colors.swift
@@ -13,5 +13,9 @@ enum Colors {
     static let lightBlue = UIColor(red: 124.0 / 255.0, green: 198.0/255.0, blue: 254.0 / 255.0, alpha: 1.0)
     static let lavander = UIColor(red: 204.0 / 255.0, green: 213.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
     static let pink = UIColor(red: 235.0 / 255.0, green:123 / 255.0, blue:192 / 255.0, alpha: 1.0)
+    
+    static let green = UIColor(red: 151.0 / 255.0 , green: 216.0 / 255.0, blue: 178.0 / 255.0, alpha: 1.0)
+    
+    static let purple = UIColor(red: 83.0 / 255.0, green: 18.0 / 255.0, blue: 83.0 / 255.0, alpha: 1.0)
 
 }
